1f26c3415090f7bb1f3719735cfcf88c26bf2b8c
Akash Bagade <akashbagade93@gmail.com>
2018-04-01 13:34:12 +0530

    1st with all changes

---
+++ app/src/main/java/com/example/akashbagade/hotel/FoodParcel.java
@@ -0,0 +1,13 @@
+package com.example.akashbagade.hotel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodParcel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_parcel);
+    }
+}
